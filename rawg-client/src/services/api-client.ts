d617abe076345fe56fcd8cd8d657e1fd6425ae2f
HaDoCK <jako7096@stud.kea.dk>
2024-10-28 21:10:33 +0100

    Updates, still issues with npm start

--- rawg-client/src/services/api-client.ts
+++ rawg-client/src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   //for the public api:"https://api.rawg.io/api/",
-  baseURL: "https://api.rawg.io/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   params: {
     key: import.meta.env.VITE_API_KEY,
   },
